24fb8101fff170a30abeec02e5ee9456f886a27e
KelvenAlvess <kelven.alves@ufrpe.br>
2026-02-26 15:59:11 -0300

    fix:webconfig

--- backend/src/main/java/com/example/marketPlace/configurations/WebConfig.java
+++ backend/src/main/java/com/example/marketPlace/configurations/WebConfig.java
@@ -1,4 +1,17 @@
 package com.example.marketPlace.configurations;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Aplica a todas as rotas
+                .allowedOrigins("*") // ⚠️ Libera para qualquer site (Vercel, Localhost, etc)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+                .allowedHeaders("*");
+    }
 }
